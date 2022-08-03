53ed13dc71db800b0bfa3488142815908cd9ae4a
luckyr13 <richie.isc@gmail.com>
2022-08-02 23:13:08 -0500

    v0.2.2 🕺

--- src/app/core/services/app-settings.service.ts
+++ src/app/core/services/app-settings.service.ts
@@ -8,7 +8,7 @@ declare const document: any;
 })
 export class AppSettingsService {
   appName = '🐘 Public Square';
-  appVersion = '0.2.1';
+  appVersion = '0.2.2';
 
   // Dev protocol
   // protocolVersion = '0.0-dev';
