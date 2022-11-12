c4dc612997f85fcd31fb10095a39bfd55180e18b
luckyr13 <richie.isc@gmail.com>
2022-11-11 20:12:42 -0600

    Add number of reposts and update labels

--- src/app/users/post/post.component.ts
+++ src/app/users/post/post.component.ts
@@ -10,10 +10,12 @@ import { NetworkInfoInterface } from 'arweave/web/network';
 import { UserProfileAddress } from '../../core/interfaces/user-profile-address';
 import { ReplyService } from '../../core/services/reply.service';
 import { LikeService } from '../../core/services/like.service';
+import { RepostService } from '../../core/services/repost.service';
 import { ViewLikesDialogComponent } from '../../shared/view-likes-dialog/view-likes-dialog.component'; 
 import { Direction } from '@angular/cdk/bidi';
 import { UserSettingsService } from '../../core/services/user-settings.service';
 import { MatDialog } from '@angular/material/dialog';
+import { ViewRepostsDialogComponent } from '../../shared/view-reposts-dialog/view-reposts-dialog.component'; 
 
 @Component({
   selector: 'app-post',
@@ -34,10 +36,15 @@ export class PostComponent implements OnInit, OnDestroy {
   @ViewChild('moreResultsCard', { read: ElementRef }) moreResultsCard!: ElementRef;
 
   public loadingLikes = false;
+  public loadingReposts = false;
   public likes: TransactionMetadata[] = [];
   private _likesSubscription: Subscription = Subscription.EMPTY;
   public maxLikes = 9;
   public maxLikesQuery = 20;
+  public reposts: TransactionMetadata[] = [];
+  private _repostsSubscription: Subscription = Subscription.EMPTY;
+  public maxReposts = 9;
+  public maxRepostsQuery = 20;
 
   constructor(
     private route: ActivatedRoute,
@@ -47,7 +54,8 @@ export class PostComponent implements OnInit, OnDestroy {
     private _reply: ReplyService,
     private _like: LikeService,
     private _userSettings: UserSettingsService,
-    private _dialog: MatDialog) { }
+    private _dialog: MatDialog,
+    private _repost: RepostService) { }
 
   ngOnInit(): void {
     this.route.parent!.data
@@ -60,6 +68,7 @@ export class PostComponent implements OnInit, OnDestroy {
         this.loadPost(userAddressList, storyId);
         this.loadReplies(storyId);
         this.loadLikes(storyId);
+        this.loadReposts(storyId);
       });
   }
 
@@ -127,7 +136,7 @@ export class PostComponent implements OnInit, OnDestroy {
   }
 
   loadLikes(storyId: string) {
-    this.loadingReplies = true;
+    this.loadingLikes = true;
     this.likes = [];
     this._repliesSubscription = this._arweave.getNetworkInfo().pipe(
       switchMap((info: NetworkInfoInterface) => {
@@ -158,6 +167,53 @@ export class PostComponent implements OnInit, OnDestroy {
 
     const dialogRef = this._dialog.open(
       ViewLikesDialogComponent,
+      {
+        restoreFocus: false,
+        autoFocus: false,
+        disableClose: false,
+        data: {
+          postId: this.post ? this.post.id : 0
+        },
+        direction: direction
+      });
+
+    dialogRef.afterClosed().subscribe(() => { 
+     
+    });
+  }
+
+  loadReposts(storyId: string) {
+    this.loadingReposts = true;
+    this.reposts = [];
+    this._repliesSubscription = this._arweave.getNetworkInfo().pipe(
+      switchMap((info: NetworkInfoInterface) => {
+        const currentHeight = info.height;
+        return this._repost.getStoryReposts(storyId, this.maxLikesQuery, currentHeight)
+      }),
+    ).subscribe({
+      next: (reposts) => {
+        for (const rp of reposts) {
+          if (!this.reposts.find(v => v.id === rp.id)) {
+            this.reposts.push(rp);
+          }
+        }
+        this.loadingReposts = false;
+      },
+      error: (error) => {
+        this.loadingReposts = false;
+        this._utils.message(error, 'error');
+      }
+    })
+  }
+
+  viewReposts() {
+    const defLang = this._userSettings.getDefaultLang();
+    const defLangObj = this._userSettings.getLangObject(defLang);
+    let direction: Direction = defLangObj && defLangObj.writing_system === 'LTR' ? 
+      'ltr' : 'rtl';
+
+    const dialogRef = this._dialog.open(
+      ViewRepostsDialogComponent,
       {
         restoreFocus: false,
         autoFocus: false,
